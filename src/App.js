96dea1ee258489de4f550785db66940f1e5f7ce0
dominthebox <dsallustro@gmail.com>
2021-07-23 15:44:47 -0400

    currently in a broken state after trying to create a PrivateRoute to the '/bubble' page in App.js. I even tried commenting out my private route but I'm still getting the error 'Can't reseolve '@potion/element' in src/components/Bubbles.js' which I didn't even touch. I did what Warren said to do in the Web44 channel before starting the project because I had that initial error trying to install npm, so I deleted the potion dependencies in package.json and did an npm i potion then npm i, it seemed to work, I got the login page working and the token, then when I tried to push the route I got this error. Not sure what to do now

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from "./components/BubblePage";
+
 function App() {
   return (
     <Router>
@@ -12,7 +15,7 @@ function App() {
           Color Picker Sprint Challenge
           <a data-testid="logoutButton" href="#">logout</a>
         </header> 
-
+        {/* <PrivateRoute path="/bubble" component={BubblePage}/> */}
         <Route exact path="/" component={Login} />
       </div>
     </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,77 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
+  const [ formValues, setFormValues ] = useState({ username: "", password: "" })
+  const [ errorMessage, setErrorMessage ] = useState("")
+
+  const { push } = useHistory();
+
+  // const error = "Username or Password not valid.";
   //replace with error state
+
+  const loginSubmit = () => {
+    axios.post(`http://localhost:5000/api/login`, formValues)
+      .then(res => {
+        // console.log(res);
+        localStorage.setItem('token', res.data.payload)
+        push("/bubble")      
+      })
+      .catch(err => {
+        console.log('Houston, we have a problem: ', err);
+      })
+  }
+
+  const handleChanges = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formValues.username === "" || formValues.password === "") {
+      setErrorMessage("Username or Password not valid.")
+    }
+    else {
+      loginSubmit();
+    }
+  };
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <h2>Login!</h2>
+        <form onSubmit={handleSubmit}>
+          <input 
+            id="username"
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={handleChanges}
+            value={formValues.username}
+          />
+          <input 
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChanges}
+            value={formValues.password}
+          />
+          <button id="submit">Login</button>
+        </form>
       </div>
 
-      <p id="error" className="error">{error}</p>
+      <p id="error" className="error">{errorMessage}</p>
     </div>
   );
 };
